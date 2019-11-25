6ff14ce270a76667b1b343a3de0ea27f531ec0f0
Daniel Dobrzensky <daniel.dobrzensky@gmail.com>
2019-11-25 10:08:13 -0800

    scroll etc

--- script.js
+++ script.js
@@ -8,3 +8,28 @@ $(".hamburger").toggleClass("is-active");
 $('#myModal').on('shown.bs.modal', function () {
   $('#myInput').trigger('focus')
 })
+
+// Begin Smooth Scroll
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "#") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+// End Smooth Scroll
